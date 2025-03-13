07135b2dc5c5888ba8d4a35428ec496e489e1460
mkanias <131133498+mkanias@users.noreply.github.com>
2025-03-13 21:16:29 +1100

    Wrote the logic required to change the cart quantity and display it on the page.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -79,9 +79,16 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         quantity: 1
       })
     }
-    
-    console.log(cart)
+
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity
+    })
+
+    document.querySelector('.js-cart-quantity')
+      .innerHTML = cartQuantity;
+
   });
 });
 
-document.querySelector
