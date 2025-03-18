479e732b816319737841c82805914043eb76554b
mkanias <131133498+mkanias@users.noreply.github.com>
2025-03-18 11:13:50 +1100

    Calculated delivery date considering no weekend delivery.

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,3 +1,5 @@
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+
 export const deliveryOptions = [{
     id: '1',
     deliveryDays: 7,
@@ -22,4 +24,32 @@ export function getDeliveryOption(deliveryOptionId) {
     });
 
     return deliveryOption || deliveryOptions[0];
+}
+
+export function calculateDeliveryDate(deliveryOption) {
+    const today = dayjs('2025-03-07');
+    let daysToAdd = deliveryOption.deliveryDays; 
+
+    let deliveryDate = today.add(
+        daysToAdd,
+        'days'
+    );
+
+    while (daysToAdd !== 0) {
+        deliveryDate = deliveryDate.add(1, 'day');
+        if (!isWeekend(deliveryDate)) {
+            daysToAdd--;
+        }
+    };
+
+    const dateString = deliveryDate.format(
+        'dddd, MMMM, D'
+    );
+
+    return dateString;
+}
+
+function isWeekend(deliveryDate) {
+    const day = deliveryDate.format('dddd');
+    return day === 'Saturday' || day === 'Sunday';
 }
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,8 +1,7 @@
 import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption } from "../../data/cart.js";
-import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { calculateDeliveryDate, deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
 import { getProduct, products } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
-import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
@@ -18,14 +17,7 @@ export function renderOrderSummary() {
 
         const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-        const today = dayjs();
-        const deliveryDate = today.add(
-            deliveryOption.deliveryDays,
-            'days'
-        );
-        const dateString = deliveryDate.format(
-            'dddd, MMMM, D'
-        );
+        const dateString = calculateDeliveryDate(deliveryOption);
 
 
         cartSummaryHTML += `
@@ -78,14 +70,7 @@ export function renderOrderSummary() {
         let html = '';
 
         deliveryOptions.forEach((deliveryOption) => {
-            const today = dayjs();
-            const deliveryDate = today.add(
-                deliveryOption.deliveryDays,
-                'days'
-            );
-            const dateString = deliveryDate.format(
-                'dddd, MMMM, D'
-            );
+            const dateString = calculateDeliveryDate(deliveryOption);
 
             const priceString = deliveryOption.priceCents
             === 0
