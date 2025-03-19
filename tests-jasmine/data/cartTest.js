862f237b57246fed8e60acb8de498ef37edb1a80
mkanias <131133498+mkanias@users.noreply.github.com>
2025-03-19 16:14:36 +1100

    Added class dependencies to the set up beforeEach, tests passing now.

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,23 +1,33 @@
 import { addToCart, cart, loadFromStorage } from '../../data/cart.js';
 
 describe('Test suite: addToCart', () => {
+    const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'; 
+    beforeEach(() => {
+        document.querySelector('.js-test-container-2').innerHTML = `
+            <select class="js-quantity-selector-${productId1}">
+              <option selected value="1">1</option>
+            </select>
+            <div class="js-added-to-cart-${productId1}"></div>
+        `;
+    })
+
     it('Adds an existing product to the cart', () => {
         spyOn(localStorage, 'setItem');
 
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([{
-                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                productId: productId1,
                 quantity: 1,
                 deliveryOptionId: '1'
             }]);
         });
         loadFromStorage();
 
-        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        addToCart(productId1);
         expect(cart.length).toEqual(1);
         expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-        expect(cart[0].productId.toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6'));
-        expect(cart[0].quantity.toEqual(2));
+        expect(cart[0].productId).toEqual(productId1);
+        expect(cart[0].quantity).toEqual(2);
     });
 
     it('Adds a new product to the cart', () => {
@@ -28,11 +38,10 @@ describe('Test suite: addToCart', () => {
         });
         loadFromStorage();
 
-        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+        addToCart(productId1);
         expect(cart.length).toEqual(1);
         expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-        expect(cart[0].productId.toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6'));
-        expect(cart[0].quantity.toEqual(1));
+        expect(cart[0].productId).toEqual(productId1);
+        expect(cart[0].quantity).toEqual(1);
     });
 });
-
