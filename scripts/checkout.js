e6d07faaa85ce583d4138d68439d3be917e252d2
mkanias <131133498+mkanias@users.noreply.github.com>
2025-03-20 22:58:27 +1100

    Converted from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,23 @@ import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js'
 
+async function loadPage() {
+    await loadProductsFetch();
+
+    await new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+
+    renderOrderSummary();
+    renderPaymentSummary();
+
+}
+
+loadPage();
+
+/* 
 Promise.all([
     loadProductsFetch(),
     
@@ -20,7 +37,7 @@ Promise.all([
     renderPaymentSummary();
 });
 
-
+ */
 // new Promise((resolve) => {
 //     loadProducts(() => {
 //         resolve('value1');
