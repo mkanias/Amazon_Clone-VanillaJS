daa4321639742b5c5fc1ed0e374258f292469d47
mkanias <131133498+mkanias@users.noreply.github.com>
2025-03-21 12:16:03 +1100

    Learned error handling for promises.

--- data/products.js
+++ data/products.js
@@ -103,16 +103,14 @@ export function loadProductsFetch() {
     });
     
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
 }
 
-loadProductsFetch().then(() => {
-  console.log('next step')
-});
 
-/* 
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -129,10 +127,15 @@ export function loadProducts(fun) {
     fun();
   });
 
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
- */
+
+
 
 // export const products = [
 //   {
